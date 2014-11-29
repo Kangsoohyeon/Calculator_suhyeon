7bf1fea11b1ac66ee99684d3846488ea572af045
KangSoohyeon <rkdtngus263@naver.com>
2014-11-29 18:18:51 +0900

    button- 을 pannel로  수정 & 종료버튼 동작

--- src/Calculator/Keypad.java
+++ src/Calculator/Keypad.java
@@ -3,107 +3,103 @@ package Calculator;
 
 import java.awt.BorderLayout;
 import java.awt.Button;
-import java.awt.Color;
 import java.awt.Frame;
 import java.awt.GridLayout;
-import java.awt.Label;
 import java.awt.Panel;
+import java.awt.TextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 
-public class Keypad extends Frame {
+public class Keypad extends Frame implements ActionListener {
+	
+	TextField display;
+	
 
 	public Keypad(){
 		setTitle("°è»ê±â");
-		
-		this.setLayout(new BorderLayout());
-		Label l = new Label("       0");
-		this.add(l,BorderLayout.NORTH);
-		
+		display = new TextField();
+		add(display,"NORTH");
+		add(buildKeypanel(),"CENTER");
 		
 		
 		
 		
 		
-		Panel p1 = new Panel(new GridLayout(5,5));
+		addWindowListener (new WindowAdapter (){
+			public void windowClosing(WindowEvent e){
+				System.exit(0);
+			}
+		});
+	}
+
 		
 		
-		Button a1 = new Button("MC");
-		Button a2 = new Button("MR");
-		Button a3 = new Button("MS");
-		Button a4 = new Button("M+");
-		Button a5 = new Button("M-");
-		p1.add(a1);
-		p1.add(a2);
-		p1.add(a3);
-		p1.add(a4);
-		p1.add(a5);
+		
+		private Panel buildKeypanel(){
+		
+		Panel panel = new Panel();
+		
+		panel.setLayout(new GridLayout(6,5));
+		panel.add(new Button ("MC"));
+		panel.add(new Button("MC"));
+		panel.add(new Button("MR"));
+		panel.add(new Button("MS"));
+		panel.add(new Button("M+"));
+		panel.add(new Button("M-"));
+		
+		panel.add(new Button("¡ç"));
+		panel.add(new Button("CE"));
+		panel.add( new Button("C"));
+		panel.add( new Button("¡¾"));
+		panel.add( new Button("¡î"));
+		
+		
+		panel.add( new Button("7"));
+		panel.add( new Button("8"));
+		panel.add(new Button("9"));
+		panel.add(new Button("/"));
+		panel.add(new Button("%"));
+		
+		
+		panel.add(new Button("4"));
+	    panel.add( new Button("5"));
+		panel.add( new Button("6"));
+		panel.add(new Button("*"));
+		panel.add(new Button("1/x"));
+		
+		
+		panel.add( new Button("1"));
+		panel.add (new Button("2"));
+		panel.add( new Button("3"));
+		panel.add( new Button("-"));
+		panel.add( new Button("="));
+		
+		
+		
+		
+		
+		panel.add( new Button("0"));
+		panel.add(new Button("."));
+		panel.add(new Button("+"));
+       
+        
 		
 	
-		
-		Button b1 = new Button("¡ç");
-		Button b2 = new Button("CE");
-		Button b3 = new Button("C");
-		Button b4 = new Button("¡¾");
-		Button b5 = new Button("¡î");
-		p1.add(b1);
-		p1.add(b2);
-		p1.add(b3);
-		p1.add(b4);
-		p1.add(b5);
-		
-		Button c1 = new Button("7");
-		Button c2 = new Button("8");
-		Button c3 = new Button("9");
-		Button c4 = new Button("/");
-		Button c5 = new Button("%");
-		p1.add(c1);
-		p1.add(c2);
-		p1.add(c3);
-		p1.add(c4);
-		p1.add(c5);
-		
-		Button d1 = new Button("4");
-		Button d2 = new Button("5");
-		Button d3 = new Button("6");
-		Button d4 = new Button("*");
-		Button d5 = new Button("1/x");
-		p1.add(d1);
-		p1.add(d2);
-		p1.add(d3);
-		p1.add(d4);
-		p1.add(d5);
-		
-		Button e1 = new Button("1");
-		Button e2 = new Button("2");
-		Button e3 = new Button("3");
-		Button e4 = new Button("-");
-		Button e5 = new Button("=");
-		p1.add(e1);
-		p1.add(e2);
-		p1.add(e3);
-		p1.add(e4);
-		p1.add(e5);
-		
-		this.add("Center",p1);
-		
-		Panel p2 = new Panel(new GridLayout(1,3));
-		Button f1 = new Button("0");
-		Button f2 = new Button(".");
-		Button f3 = new Button("+");
-        p2.add(f1);
-        p2.add(f2);
-        p2.add(f3);
-        
-        this.add("South",p2);
-        
-        setSize(300,400);
-		setVisible(true);
+	
+		return panel;
 		
 		
-		
-		
-		
-		
-		
-	}
+}
+
+
+
+
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
 }
